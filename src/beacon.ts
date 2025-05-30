6b915872fa20c742b7c007af91375971c9bee66d
r-near <163825889+r-near@users.noreply.github.com>
2025-05-29 22:10:13 -0700

    viem and lodestar

---
+++ src/beacon.ts
@@ -0,0 +1,20 @@
+import { getClient } from "@lodestar/api";
+import { sepoliaChainConfig } from "@lodestar/config/networks";
+import type {
+    LightClientUpdate,
+} from "@lodestar/types";
+import { createChainForkConfig } from "@lodestar/config";
+
+const config = createChainForkConfig(sepoliaChainConfig)
+const api = getClient({ baseUrl: "http://unstable.sepolia.beacon-api.nimbus.team/" }, { config });
+
+const response = await api.lightclient.getLightClientUpdatesByRange({
+    startPeriod: 941,
+    count: 1,
+})
+const update: LightClientUpdate[] = response.value();
+console.log(update)
+
+const syncing = await api.node.getSyncingStatus()
+console.log(syncing.value().isSyncing)
+
---
+++ src/execution.ts
@@ -0,0 +1,9 @@
+import { http, createPublicClient } from 'viem'
+import { sepolia } from 'viem/chains'
+
+export const publicClient = createPublicClient({
+    chain: sepolia,
+    transport: http(),
+})
+const block = await publicClient.getBlock()
+console.log(block)
